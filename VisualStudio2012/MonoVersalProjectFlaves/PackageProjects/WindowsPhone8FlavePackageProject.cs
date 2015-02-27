3915bb3cbe18b0948564565f5dc29071849dcb4a
moljac <mcvjetko@holisticware.net>
2015-02-27 20:19:06 +0100

    vs2012 added

---
+++ VisualStudio2012/MonoVersalProjectFlaves/PackageProjects/WindowsPhone8FlavePackageProject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell.Flavor;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace HolisticWare.MonoVersalFlave2010
+{
+	[ComVisible(true)]
+	[ClassInterface(ClassInterfaceType.None)]
+	[Guid(PackageProjectGuid)]
+	public class WindowsPhine8FlavePackageProject : FlavoredProjectBase
+	{
+		// To initialize GUID in Attribute (above)
+		private const string PackageProjectGuid = WindowsPhine8Project.Guid;
+
+		private MonoVersalProjectFlavesForVisualStudio2010 package;
+
+		public WindowsPhine8FlavePackageProject(MonoVersalProjectFlavesForVisualStudio2010 package)
+		{
+			this.package = package;
+		}
+
+		protected override void SetInnerProject(IntPtr innerIUnknown)
+		{
+			if (base.serviceProvider == null)
+			{
+				base.serviceProvider = this.package;
+			}
+
+			base.SetInnerProject(innerIUnknown);
+		}
+
+	}
+}
---
+++ VisualStudio2012/MonoVersalProjectFlaves/ProjectFactories/CompactFrameworkSmartDeviceProjectFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.ComponentModel.Design;
+using Microsoft.VisualStudio.Shell.Flavor;
+using Microsoft.Win32;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+
+namespace HolisticWare.MonoVersalFlave2010
+{
+	[ComVisible(false)]
+	[Guid(PackageProjectGuid)]
+	public class CompactFrameworkSmartDeviceFlavorProjectFactory : FlavoredProjectFactoryBase
+	{
+		// To initialize GUID in Attribute (above)
+		private const string PackageProjectGuid = CompactFrameworkSmartDeviceProject.Guid;
+
+		private MonoVersalProjectFlavesForVisualStudio2010 package;
+
+		public CompactFrameworkSmartDeviceFlavorProjectFactory(MonoVersalProjectFlavesForVisualStudio2010 package)
+			: base()
+		{
+			this.package = package;
+		}
+
+		protected override object PreCreateForOuter(IntPtr outerProjectIUnknown)
+		{
+			return new CompactFrameworkSmartDeviceFlavePackageProject(this.package);
+		}
+	}
+
+}
